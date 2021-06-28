1a01b59c378aae5990cc19e3c0e5b57aef861c3e
Aadarsh Patel <71113410+aadarshpatel17@users.noreply.github.com>
2021-06-28 13:32:06 +0530

    Update game.js

--- game.js
+++ game.js
@@ -59,7 +59,7 @@ getNewQuestion = () => {
   if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
     localStorage.setItem("mostRecentScore", score);
     // go to the end page
-    return window.location.assign("/end.html");
+    return window.location.assign("https://aadarshpatel17.github.io/quickquiz/end.html");
   }
 
   questionCounter++;
